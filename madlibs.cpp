e56992a0cc6d34d6b871d6aa9f23031b8162f4b6
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-09-09 17:14:08 -0700

    Describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -4,6 +4,22 @@
 
 int main()
 {
+       
+using namespace std;
+
+
+        string name = "";
+        string verb = "";
+        int number = ' ';
+        string plural_noun = "";
+
+        cout << "A long time ago there lived a" << name << endl;
+        cout << "They spent their days" << verb << endl;
+        cout << "One day, intruders arrived, almost" << number << endl;
+        cout << plural_noun << " had attacked them." << endl;
+
+
+
 
   return 0;
 }
